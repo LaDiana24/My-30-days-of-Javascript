7a5bfdd27d52ce629974356b8f8c78f1a43d84df
LaDiana24 <91518465+LaDiana24@users.noreply.github.com>
2024-05-09 23:55:03 +0800

    Add files via upload

---
+++ Day02_Day_Data_Types/string_methods/includes.js
@@ -0,0 +1,15 @@
+// includes(): It takes a substring argument and it check if substring argument exists in the string. includes() returns a boolean. It checks if a substring exist in a string and it returns true if it exists and false if it doesn't exist.
+
+let string = '30 Days of JavaScript'
+console.log(string.includes('Days'))        // true
+console.log(string.includes('days'))        // false
+console.log(string.includes('Script'))      // true
+console.log(string.includes('script'))      // false
+console.log(string.includes('java'))        // false
+console.log(string.includes('Java'))        // true
+
+let country = 'Indonesia'
+console.log(country.includes('indo'))        // false
+console.log(country,includes('Indo'))        // true
+console.log(country.includes('nesia'))       // true
+console.log(country.includes('Nesia'))       // false
